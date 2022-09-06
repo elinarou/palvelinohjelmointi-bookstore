cac399dd9f9c7cbd2a295c299ba3a4a23280cb78
elinarou <elina_rouvinen@outlook.com>
2022-09-06 11:36:56 +0300

    First commit

---
+++ src/main/java/s22/Bookstore/web/BookController.java
@@ -0,0 +1,14 @@
+package s22.Bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+
+	@GetMapping("/index")
+	public String showBooks(Model model) {
+		return "index";
+	}
+}
